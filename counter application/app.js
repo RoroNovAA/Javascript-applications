584f4543f93ab082fc8f474d21c230d7c8ae3415
Ketan Sahu <rorozoro9888@gmail.com>
2023-04-19 17:36:31 +0530

    finished the counter app

--- counter application/app.js
+++ counter application/app.js
@@ -1,6 +1,26 @@
-const decrement = document.getElementsByClassName("decrement-button");
-const increment = document.getElementsByClassName('increment-button');
-const displayNumber = document.getElementsByClassName('number-input');
-const reset = document.getElementsByClassName('reset-button');
+/* BUTTONS AND INPUT */
+const decrementButton = document.querySelector(".decrement-button");
+const incrementButton = document.querySelector(".increment-button");
+const resetButton = document.querySelector(".reset-button");
+const input = document.querySelector(".display-value");
 
+/* DEFAULT COUNTER VALUE */
+let counter = 0;
+
+
+/* ADDING EVENT LISTENERS */
+decrementButton.addEventListener("click", () => {
+    counter -= 1;
+    input.innerText = counter;
+})
+
+incrementButton.addEventListener("click", () => {
+    counter += 1;
+    input.innerText = counter;
+})
+
+resetButton.addEventListener("click", () => {
+    counter = 0;
+    input.innerText = counter;
+})
 
