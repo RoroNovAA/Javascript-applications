b59b7789151178e89af0182e1c684105f1d1b0fd
Ketan Sahu <rorozoro9888@gmail.com>
2023-04-19 16:55:55 +0530

    first commit

---
+++ counter application/app.js
@@ -0,0 +1,6 @@
+const decrement = document.getElementsByClassName("decrement-button");
+const increment = document.getElementsByClassName('increment-button');
+const displayNumber = document.getElementsByClassName('number-input');
+const reset = document.getElementsByClassName('reset-button');
+
+
